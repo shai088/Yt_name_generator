0e3efd3d29711e87680d60ce192b08d23ed36f79
Chaimae.bk <sha663277@gmail.com>
2025-08-27 16:44:06 +0100

    Add files via upload

---
+++ yt name.py
@@ -0,0 +1,4 @@
+print("Welcome to the YouTube Channel Name Generator: ")
+name = input('what is your nickname? \n')
+topic = input('what is your channel about? \n')
+print("You could name your channel " + "(" + topic + " with " + name + ")")
